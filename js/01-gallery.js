50a640c5d1c9a941d9acd60acb4aa1ab6f9374ef
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-02 19:20:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const imageList = document.querySelector("gallery");
+
+const imageItem = galleryItems.map(
+  ({original, description }) => `<li class="gallery_item">
+<img class="newImage" src="${original}" alt="${description}">
+</li>`).join("");
+
+newImageList.insertAdjacentHTML("afterbegin", imageItem);
