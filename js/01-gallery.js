f4ff9b7754b36d44dd8fd6cb38570875a4182eb4
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-06 01:12:57 +0300

    Финиш

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 
-const imageList = document.querySelector("gallery");
 
-const imageItem = galleryItems.map(
-  ({original, description }) => `<li class="gallery_item">
-<img class="newImage" src="${original}" alt="${description}">
-</li>`).join("");
+const galleryList = document.querySelector(".gallery");
 
-newImageList.insertAdjacentHTML("afterbegin", imageItem);
+const galleryItemsList = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", galleryItemsList);
+
+
+
+
+
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600" alt="${event.target.alt}"/>
+    `,
+    {
+      onShow: () => document.addEventListener("keydown", onEscPush),
+      onClose: () => document.removeEventListener("keydown", onEscPush)
+    }
+  );
+  instance.show();
+
+  function onEscPush (event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+});
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsList = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", galleryItemsList);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  
+});
